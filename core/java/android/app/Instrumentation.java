f6dfbaca5ddf4b5fa733657c96bd7341a39792d8
Wind/owZ <windowz414@gnuweeb.org>
2021-10-14 20:21:34 +0000

    Revert "gmscompat: Add compatibility layer for unprivileged Google Play services"
    This reverts commit c0e5314635944ac163cd5e6e4c06fa4419282280.

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -57,8 +57,6 @@ import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
 
-import com.android.internal.gmscompat.GmsHooks;
-
 import java.io.File;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -1178,7 +1176,6 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        GmsHooks.initApplicationBeforeOnCreate(app);
         return app;
     }
     
@@ -1196,7 +1193,6 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        GmsHooks.initApplicationBeforeOnCreate(app);
         return app;
     }
 
